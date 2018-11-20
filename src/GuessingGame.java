0a5ab1e6fb788d8faacbd803e930788924d2852b
Daniella Noren <daniellanoren@live.se>
2018-11-20 21:03:10 +0100

    Add userinput to GuessingGame, add equalsIgnoreCase to doGuesses()

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -3,7 +3,15 @@ public class GuessingGame {
 
 	public static void main(String[] arg) {
 		
-		Guesser guesser = new Guesser(1, 1000);
+		int low = 0;
+		int high = 0;
+		try {
+			low = Integer.parseInt(arg[0]);
+			high = Integer.parseInt(arg[1]);
+		}catch(NumberFormatException e) {
+			System.out.println("Not an int");
+		}
+		Guesser guesser = new Guesser(low, high);
 		guesser.start();
 		
 		
